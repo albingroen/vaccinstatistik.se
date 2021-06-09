850f89a16a12366446ea298a19673feabd7a1023
Albin Groen <albin.groen@gmail.com>
2021-06-09 08:39:15 +0200

    :wrench: Add splitbee

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import "../styles/globals.css";
+import splitbee from "@splitbee/web";
+
+// This initiliazes Splitbee.js
+splitbee.init();
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
