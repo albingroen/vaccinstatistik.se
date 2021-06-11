2e81896ad3e1872b77b1b6ec0cdd930eb94ca899
Albin Groen <albin.groen@gmail.com>
2021-06-11 15:41:34 +0200

    :sparkles: Show percentage value next to progress bar

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -5,11 +5,12 @@ interface Progress {
 
 export default function Progress({ value, className }: Progress) {
   return (
-    <div
-      className="w-full h-2 overflow-hidden bg-gray-200 rounded-full"
-      title={`${Math.round(value)}%`}
-    >
-      <div className={`h-full ${className}`} style={{ width: `${value}%` }} />
+    <div className="flex items-center space-x-4">
+      <div className="w-full h-2 overflow-hidden bg-gray-200 rounded-full">
+        <div className={`h-full ${className}`} style={{ width: `${value}%` }} />
+      </div>
+
+      <span className="text-gray-400">~{Math.round(value)}%</span>
     </div>
   );
 }
