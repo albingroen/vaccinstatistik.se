0c5e7ad011e6301db62c59a45f36fa24e3ccb0bd
Albin Groen <albin.groen@gmail.com>
2021-06-12 09:18:20 +0200

    :recycle: Add ~ character before percentage values

--- pages/index.tsx
+++ pages/index.tsx
@@ -256,7 +256,7 @@ export default function Home() {
                     { payload: { shareAtLeast1, share } }
                   ) => {
                     return [
-                      `${value.toLocaleString()} (${Math.round(
+                      `${value.toLocaleString()} (~${Math.round(
                         (type === "Minst 1 dos" ? shareAtLeast1 : share) * 100
                       )}%)`,
                       "Antal",
