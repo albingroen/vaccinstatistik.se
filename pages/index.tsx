235f048d5f88a9ade3d95920574efdfe1aaebbf6
Albin Groen <albin.groen@gmail.com>
2021-07-10 17:47:21 +0200

    :mag: Add keywords

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,6 +52,7 @@ export default function Home() {
         <title>{title}</title>
         <meta name="title" content={title} />
         <meta name="description" content={description} />
+        <meta name="keywords" content="vaccinstatistik, vaccin, vaccination" />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content={url} />
